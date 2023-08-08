74918449d6314760698aab4c49b73123dca1962d
WillMcdaniel <125214391+WillMcdaniel@users.noreply.github.com>
2023-08-07 20:50:01 -0700

    Initial commit

--- main.py
+++ main.py
@@ -1,43 +1,50 @@
+import os
+from dotenv import load_dotenv
+import threading
+from queue import Queue
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 import requests
-
+load_dotenv()
 app = Flask(__name__)
 
-# This can be a database or a more complex data structure
-pending_requests = {}
-
+# Using a Queue to process incoming requests
+pending_requests = Queue()
+def handle_requests():
+    while True:
+        number, business_type = pending_requests.get()
 def format_places_data(data):
-    stores = data.get('results', [])[:5]  # Limit to the top 5 results
-    messages = []
+            stores = data.get('results', [])[:5]  # Limit to the top 5 results
+            messages = []
 
-    for store in stores:
-        name = store.get('name', "No name provided")
-        address = store.get('vicinity', "No address provided")
-        open_now = store.get('opening_hours', {}).get('open_now', False)
-        status = "Open" if open_now else "Closed"
-        message = f"{name} at {address} is currently {status}."
-        messages.append(message)
+            for store in stores:
+                name = store.get('name', "No name provided")
+                address = store.get('vicinity', "No address provided")
+                open_now = store.get('opening_hours', {}).get('open_now', False)
+                status = "Open" if open_now else "Closed"
+                message = f"{name} at {address} is currently {status}."
+                messages.append(message)
 
-    return "\n".join(messages)
+            return "\n".join(messages)
+            pending_requests.task_done()
 
+threading.Thread(target=handle_requests).start()
+API_KEY = os.environ.get('API_KEY')
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-    # Get the message the user sent our Twilio number
+    # Get the message the user sent to the Twilio number
     body = request.values.get('Body', None)
 
     # Get the phone number the message was sent from
     from_number = request.values.get('From', None)
 
-    API_KEY = 'AIzaSyBiCvlp6HgO8SpEBYUSEYIJRcNqFTw2AyQ'
-
     # Start our Twilio response
     resp = MessagingResponse()
 
-    if from_number in pending_requests:
+    if pending_requests.get(from_number, None):
         # The user has already made a request and is now providing their address
         address = body
-        business_type = pending_requests[from_number]  # Add this line
+        business_type = pending_requests.get(from_number)
 
         # Convert the address to coordinates (using the Google Geocoding API)
         params = {
@@ -76,14 +83,13 @@ def sms_reply():
         else:
             response_message = f"Couldn't geocode the provided address: {address}"
 
-        # Remove the pending request
-        #del pending_requests[from_number]
+
     else:
         # This is a new request
         business_name = body.lower()
 
         # Store the request as pending
-        pending_requests[from_number] = business_name
+        pending_requests.put((from_number, business_name))
 
         response_message = "Please reply with your address."
 
@@ -93,6 +99,7 @@ def sms_reply():
 
     return str(resp)
 
+pending_requests.join()
 
 if __name__=="__main__":
     app.run(debug=True, port=8080)
